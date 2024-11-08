8b06549c9d161e3b6a849211def04995a3ddd538
Syedowais16 <117106667+Syedowais16@users.noreply.github.com>
2024-11-08 12:52:41 +0500

    Add files via upload

---
+++ lab-task/server/controllers/authController.js
@@ -0,0 +1,36 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: 'Invalid email or password' });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ message: 'Invalid email or password' });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.json({ message: 'Login successful', token });
+  } catch (error) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+const register = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        return res.status(400).json({ message: 'User already exists' });
+      }
+  
+      const user = new User({ email, password });
+      await user.save();
+      res.json({ message: 'User registered successfully' });
+    } catch (error) {
+      res.status(500).json({ message: 'Server error' });
+    }
+  };
+module.exports = { login , register};
